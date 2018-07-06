095bb4461b169e2a61c26831f7acb759ade35362
Build automation <miikka.koskinen+ci@iki.fi>
2018-07-06 15:29:30 +0000

    Version 0.1.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cache-metrics "0.1.1"
+(defproject cache-metrics "0.1.2-SNAPSHOT"
   :description "Instrumentation for core.cache"
   :url "https://github.com/miikka/cache-metrics"
   :license {:name "Eclipse Public License"
