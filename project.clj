cb3302d0dca84c541420eaaff4432e7de96f5df5
Build automation <miikka.koskinen+ci@iki.fi>
2020-01-07 17:01:41 +0000

    Version 0.1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cache-metrics "0.1.3-SNAPSHOT"
+(defproject cache-metrics "0.1.3"
   :description "Instrumentation for core.cache"
   :url "https://github.com/miikka/cache-metrics"
   :scm {:name "git", :url "https://github.com/miikka/cache-metrics"}
