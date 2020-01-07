6d11903fcd702e8bb15d62d1b20ef0d6719797e2
Miikka Koskinen <miikka@metosin.fi>
2020-01-07 17:37:34 +0200

    Upgrade dependencies

--- project.clj
+++ project.clj
@@ -1,12 +1,13 @@
 (defproject cache-metrics "0.1.2-SNAPSHOT"
   :description "Instrumentation for core.cache"
   :url "https://github.com/miikka/cache-metrics"
+  :scm {:name "git", :url "https://github.com/miikka/cache-metrics"}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :deploy-repositories [["releases" :clojars]]
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/core.cache "0.6.5"]
-                 [io.dropwizard.metrics/metrics-core "3.2.2"]]
+  :dependencies [[org.clojure/core.cache "0.8.2"]
+                 [io.dropwizard.metrics/metrics-core "4.1.2"]]
   :plugins [[lein-auto "0.1.3"]
             [lein-eftest "0.5.2"]]
-  :profiles {:dev {:source-paths ["dev-src"]}})
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]]
+                   :source-paths ["dev-src"]}})
